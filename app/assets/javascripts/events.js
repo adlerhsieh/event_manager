5bef310d8bca2d94d6c24f4a1153dce6ae229f2b
Adler <nkj20932@hotmail.com>
2016-04-07 22:55:23 +0800

    add ajax kaminari

---
+++ app/assets/javascripts/events.js
@@ -0,0 +1,8 @@
+// $(document).ready(function(){
+//   $("a[data-remote]").click(function(e){
+//     e.preventDefault();
+//     var url = $(this).attr("href");
+//     $.getScript(url, function(data, textStatus, jqxhr){
+//     });
+//   });
+// });
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,80 +1,76 @@
 class EventsController < ApplicationController
   before_action :find_event, only: [:show, :edit, :update, :dashboard, :join, :withdraw]
-  before_action :authticate_user!, only: [:new, :edit]
 
-  def index
-    if params[:search]
-      @events = Event.where("name LIKE ?", "%#{params[:search]}%")
-    else
-      @events = Event.all
-    end
-    # if params[:order] == 'name'
-    #   sort_by = 'name'
-    # else
-    #   sort_by = 'created_at'
-    # end
-    # @events = Event.order(sort_by)
-  end
+      def index
+         @events = Event.includes(:groups, :location, :category).all
+         @events.search(params[:search]) if params[:search]
+         @events = @events.page(params[:page]).per(5)
 
-  def new
-    @event = Event.new
-  end
+         respond_to do |format|
+           format.html
+           format.js
+         end
+      end
 
-  def edit
+      def new
+        @event = Event.new
+      end
 
-  end
+      def edit
 
-  def show
+      end
 
-  end
+      def show
 
-  def latest
-    @events = Event.order(:created_at).limit(2)
-  end
+      end
 
-  def join
-    Membership.find_or_create_by(:event => @event, :user => current_user)
-    redirect_to :back
-  end
+      def latest
+        @events = Event.order(:created_at).limit(2)
+      end
 
-  def withdraw
-    @membership = Membership.find_by(:event => @event, :user => current_user)
-    @membership.destroy if @membership
-    redirect_to :back
-  end
+      def join
+        Membership.find_or_create_by(:event => @event, :user => current_user)
+        redirect_to :back
+      end
 
-  def bulk_delete
-    Event.destroy_all
-    redirect_to events_path
-  end
+      def withdraw
+        @membership = Membership.find_by(:event => @event, :user => current_user)
+        @membership.destroy if @membership
+        redirect_to :back
+      end
 
-  def dashboard
-  end
+      def bulk_delete
+        Event.destroy_all
+        redirect_to events_path
+      end
 
-  def create
-    @event = Event.new(event_params)
-    if @event.save
-      redirect_to events_path
-    else
-      render action: :new
-    end
-  end
+      def dashboard
+      end
 
-  def update
-    if @event.update(event_params)
-      redirect_to event_path(@event)
-    else
-      render action: :edit
-    end
-  end
+      def create
+        @event = Event.new(event_params)
+        if @event.save
+          redirect_to events_path, warning: 'warning!'
+        else
+          render action: :new
+        end
+      end
 
-  private
+      def update
+        if @event.update(event_params)
+          redirect_to event_path(@event)
+        else
+          render action: :edit
+        end
+      end
 
-  def find_event
-    @event = Event.find(params[:id])
-  end
+      private
 
-  def event_params
-    params.require(:event).permit(:name, :description, :category_id, :location_attributes => [:id, :name, :_destroy], :group_ids => [] )
-  end
+      def find_event
+        @event = Event.find(params[:id])
+      end
+
+      def event_params
+        params.require(:event).permit(:name, :description, :category_id, :location_attributes => [:id, :name, :_destroy], :group_ids => [] )
+      end
 end
